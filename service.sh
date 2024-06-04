60d80f1a8fd2340d9155493c0db68c2b090ec655
MONE FIERA <83483662+monefiera@users.noreply.github.com>
2024-06-04 11:52:04 +0900

    Update service.sh

--- service.sh
+++ service.sh
@@ -9,18 +9,27 @@ MODDIR=${0%/*}
 # This script will be executed in late_start service mode
 
 echo "1" > /proc/gesture_control
-echo "4200000" > /sys/class/power_supply/battery/voltage_max
 pm disable-user --user 0 com.android.providers.contacts
 pm disable-user --user 0 com.android.nfc
+pm uninstall --user 0 com.android.smspush
 pm uninstall --user 0 me.phh.treble.overlay.cafims_telephony
 pm uninstall --user 0 me.phh.treble.overlay.hwims_telephony
+pm uninstall --user 0 me.phh.treble.overlay.mtkims_telephony
 pm uninstall --user 0 me.phh.treble.overlay.slsiims_telephony
 pm uninstall --user 0 me.phh.treble.overlay.sprdims_telephony
 pm uninstall --user 0 me.phh.treble.overlay.telephony.lte
 pm uninstall --user 0 me.phh.treble.overlay.tethering
-pm uninstall --user 0 com.google.android.keep
+pm uninstall --user 0 com.android.mms.service
+pm uninstall --user 0 com.android.stk
 pm uninstall --user 0 com.android.simappdialog
-pm uninstall --user 0 com.android.cellbroadcastreceiver
-pm uninstall --user 0 com.android.smspush
-pm uninstall --user 0 com.android.cellbroadcastservice
-pm uninstall --user 0 com.android.calllogbackup
+pm uninstall --user 0 com.android.providers.blockednumber
+pm uninstall --user 0 com.android.messaging
+pm uninstall --user 0 com.android.cellbroadcastreceiver.module
+pm uninstall --user 0 com.android.carrierdefaultapp
+pm uninstall --user 0 com.android.server.telecom
+pm uninstall --user 0 com.android.dialer
+pm uninstall --user 0 com.android.phone
+pm uninstall --user 0 com.android.contacts
+pm uninstall --user 0 im.angry.openeuicc
+pm uninstall --user 0 com.google.android.dialer
+pm uninstall --user 0 com.google.android.contacts
